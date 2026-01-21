655c34893be37acebcaefe7d5a22ca6e172734d4
Eunha <galaxy.eunha.sim@gmail.com>
2026-01-21 14:51:55 -0500

    keep SDlogging file open instead of open and closing at 5Hz and 1Hz

--- SDCardModule.cpp
+++ SDCardModule.cpp
@@ -1,6 +1,7 @@
 #include "SDCardModule.h"
 
 File myFile;
+bool sdReady = false;
 
 bool initSDCard(uint8_t csPin) {
     if (!SD.begin(csPin)) {
@@ -8,6 +9,15 @@ bool initSDCard(uint8_t csPin) {
         return false;
     }
     Serial.println(F("SD card initialized."));
+
+    myFile = SD.open("log.txt", FILE_WRITE);
+    if (!myFile){
+        Serial.println(F("Failed to open log.txt for writing!"));
+        sdReady = false;
+        return false;
+    }
+
+    sdReady = true;
     return true;
 }
 
